a453b816a7e76410e7fea4735406e84cbec973ac
Martin Lenart <martin_lenart@icloud.com>
2021-09-09 12:41:30 +0200

    Example Update

--- BOOP_02_01/Program.cs
+++ BOOP_02_01/Program.cs
@@ -22,8 +22,10 @@ namespace BOOP_02_01
             int x = 1;
             int y = 2;
 
+            bool isEqual = x == y;
+
             // Equality based on value
-            Console.WriteLine(x == y);      // False
+            Console.WriteLine(isEqual);      // False
             Console.WriteLine(x != y);      // True
             Console.WriteLine(x < y);       // True
             Console.WriteLine(x >= y);      // False
@@ -45,7 +47,6 @@ namespace BOOP_02_01
             // The && and || operators and & and | test for and and or conditions. 
             cPoint3 = new classPoint { X = 10, Y = 10 };
             //cPoint3 = null;
-
             // &&, || short circuit
             if (cPoint3 != null && cPoint3.X > 0)
                 Console.WriteLine("cPoint3.X is > 0");
@@ -53,10 +54,12 @@ namespace BOOP_02_01
                 Console.WriteLine("cPoint3 is null");
 
             // &, | do not short cicuit and whole expression is evaluated
+            /*
             if (cPoint3 != null & cPoint3.X > 0)                    // Exception is thrown when cPoint3 == null
                 Console.WriteLine("cPoint3.X is > 0");
             else
                 Console.WriteLine("cPoint3 is null");
+            */
         }
     }
 }
